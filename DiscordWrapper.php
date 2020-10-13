05aec3717502becac8448317c779a88760747391
Alberto González <alberto@gcatalan.com>
2020-10-13 11:02:15 +0000

    First concept

---
+++ DiscordWrapper.php
@@ -0,0 +1,21 @@
+class DiscordWrapper
+{
+
+    protected $discord;
+    protected $webhook;
+
+    // constructor con url de webhook
+    public function __construct()
+    {
+        765528404036419614/6lhV6OVE11w0Hj-3pB1m3aXQ9GD4B_CJH5f82SsMQENNp6ftqzeWV_AlsfOBvsggkUhf
+    }
+
+    /*
+    * funcion para enviar mensaje
+    */
+    public function sendToDiscord()
+    {
+        $url = 'https://discordapp.com/api/webhooks/';
+    }
+
+}
